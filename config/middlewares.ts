6327a411bfb1a91bffe32794e739d77db46c07dd
Jonathan Kaemper <jonathan@chainovate.com>
2025-11-13 00:43:05 +0100

    add strapi-plugin-email-designer-v5

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,19 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "script-src": ["'self'", "'unsafe-inline'", "editor.unlayer.com"],
+          "frame-src": ["'self'", "editor.unlayer.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +21,5 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+
 ];
